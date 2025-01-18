387f8496e81ea87f50dd099144ed2d64fbadfde4
bhavneet verma <bhavneetv295@gmail.com>
2025-01-18 17:26:49 +0530

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -8,7 +8,7 @@ let userMessage = null; // Variable to store user's message
 const inputInitHeight = chatInput.scrollHeight;
 
 // API configuration
-const API_KEY = "AIzaSyAAwoT5j8Uk7dMONe0WIzxWpMtCbgaELDs"; // Your API key here
+const API_KEY = ""; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const createChatLi = (message, className) => {
